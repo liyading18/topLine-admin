4a294b03c8db9cf62569eedf372762e17a37b06c
AngeviLee <568178364@qq.com>
2019-06-22 12:34:06 +0800

    安装配置element

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 引入element组件
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 // 优先查找文件，如果文件找不到则找目录
 // 找到目录，优先加载目录中的index.js
 import router from './router'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
