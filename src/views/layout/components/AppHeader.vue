2a32a13e96edf395b55c476976b70b2308dc82d2
AngeviLee <568178364@qq.com>
2019-06-24 00:16:12 +0800

    Layout组件-Header布局

--- src/views/layout/components/AppHeader.vue
+++ src/views/layout/components/AppHeader.vue
@@ -1,7 +1,19 @@
 <template>
-  <div>
-    <p>AppHeader Component</p>
-  </div>
+  <el-row class="app-header">
+    <el-col :span="14">中国中科院自动化科技有限责任公司</el-col>
+    <el-col :span="4" :offset="6">
+      <el-dropdown trigger="click">
+        <span class="el-dropdown-link">
+          <img src="http://toutiao.meiduo.site/Fkj6tQi3xJwVXi1u2swCElotfdCi" width="30">
+          15931441062<i class="el-icon-arrow-down el-icon--right"></i>
+        </span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>账户设置</el-dropdown-item>
+          <el-dropdown-item>退出</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
@@ -16,4 +28,23 @@ export default {
 </script>
 
 <style lang='less' scoped>
+.app-header {
+  height: 60px;
+  display: flex;
+  align-items: center;
+}
+
+.el-dropdown-link {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  color: #409EFF;
+  img {
+    border-radius: 50%;
+  }
+
+  .el-icon-arrow-down {
+    font-size: 12px;
+  }
+}
 </style>
