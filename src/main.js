74476e340c05071d067f1358327fd0bccb3c2baf
AngeviLee <568178364@qq.com>
2019-06-25 12:07:23 +0800

    使用axios响应拦截器统一处理401状态码防止伪造令牌

--- src/main.js
+++ src/main.js
@@ -21,13 +21,17 @@ import axios from 'axios'
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
 // axios请求拦截器
 // 所有使用axios发起的请求都要经过这里
-// return config 即使允许通过的方式
 // config是本次请求相关的配置
 axios.interceptors.request.use(config => {
   const userInfo = JSON.parse(window.localStorage.getItem('user_info'))
   // console.log('有请求经过了')
   // console.log(config)
-  config.headers.Authorization = `Bearer ${userInfo.token}`
+  // 如果登录了，才给那些需要token的接口统一添加token令牌
+  // 登录相关接口不需要添加token令牌，想要也没有
+  if (userInfo) {
+    config.headers.Authorization = `Bearer ${userInfo.token}`
+  }
+  // return config 即使允许通过的方式
   return config
 }, function(error) {
   return Promise.reject(error)
@@ -41,7 +45,18 @@ axios.interceptors.response.use(response => {
   return response.data.data
 }, error => {
   // >=400会进入这里
-  // console.log('response error=>', response)
+  // 查看错误对象  dir只能单个使用，不能在一行
+  // console.log('response error=>')
+  // console.dir(error)
+  const status = error.response.status
+  if (status === 401) {
+    // 过期的token或伪造的token,删除本地存储的用户信息数据
+    window.localStorage.removeItem('user_info')
+    // 跳转到登录页面
+    router.push({
+      name: 'login'
+    })
+  }
   return Promise.reject(error)
 })
 
