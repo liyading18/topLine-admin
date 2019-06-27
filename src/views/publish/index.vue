e4029dae28d3f988c1ab1e439afc88f995f96482
AngeviLee <568178364@qq.com>
2019-06-28 00:33:20 +0800

    发布文章-配置使用富文本编辑器

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -9,25 +9,24 @@
     </div>
     <el-form>
       <!-- 文章标题不能少于5个字符 -->
-      <el-form-item label="标题">
-        <el-input type="text" v-model="articleForm.title"></el-input>
+      <el-form-item>
+        <el-input type="text" v-model="articleForm.title" placeholder="标题"></el-input>
       </el-form-item>
-      <el-form-item label="内容">
-        <el-input type="textarea" v-model="articleForm.content"></el-input>
+      <el-form-item>
+        <!-- git中的富文本编辑器 -->
+        <quill-editor v-model="articleForm.content" ref="myQuillEditor" :options="editorOption"></quill-editor>
       </el-form-item>
       <el-form-item label="封面"></el-form-item>
       <el-form-item label="频道">
         <!-- 给频道组件一个出口 -->
         <!-- 利用组件通信 -->
         <!-- 父传值: Props Down
-        子传父: Events Up -->
+        子传父: Events Up-->
         <!-- <article-channel
           :value="articleForm.channel_id"
           @input="articleForm.channel_id=$event"
-        ></article-channel> -->
-        <article-channel
-          v-model="articleForm.channel_id"
-        ></article-channel>
+        ></article-channel>-->
+        <article-channel v-model="articleForm.channel_id"></article-channel>
       </el-form-item>
     </el-form>
   </el-card>
@@ -36,11 +35,18 @@
 <script>
 // 引入频道组件
 import ArticleChannel from '@/components/article-channel'
+// 引入富文本编辑器css样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+// 引入富文本编辑器
+import { quillEditor } from 'vue-quill-editor'
 export default {
   name: 'AppPublish',
   // 配置频道组件选项
   components: {
-    ArticleChannel
+    ArticleChannel,
+    quillEditor
   },
   data() {
     return {
@@ -58,8 +64,19 @@ export default {
         },
         // 频道,自己设置它默认是空字符串
         channel_id: ''
-      }
+      },
+      // 富文本编辑器相关参数选项
+      editorOption: {}
     }
+  },
+  // 计算属性
+  computed: {
+    editor() {
+      return this.$refs.myQuillEditor.quill
+    }
+  },
+  mounted() {
+    console.log('this is current quill instance object', this.editor)
   },
   methods: {
     handlePublish(draft = false) {
