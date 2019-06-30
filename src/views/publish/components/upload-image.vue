f890e4ddc91a254009b86dccc3cf53dcd2d1358e
AngeviLee <568178364@qq.com>
2019-07-01 00:58:16 +0800

    封面处理-上传图片

--- src/views/publish/components/upload-image.vue
+++ src/views/publish/components/upload-image.vue
@@ -9,7 +9,25 @@
     </div>
     <!-- 对话框 -->
     <el-dialog title="素材" :visible.sync="dialogVisible" width="30%">
-      <span>这是一段信息</span>
+      <!-- element组件中tab标签 -->
+      <el-tabs value="second">
+        <el-tab-pane label="素材库" name="first">素材库</el-tab-pane>
+        <el-tab-pane label="上传图片" name="second">
+          <!-- element组件库上传组件 -->
+          <el-upload
+            class="avatar-uploader"
+            action="http://ttapi.research.itcast.cn/mp/v1_0/user/images"
+            :show-file-list="false"
+            name="image"
+            :headers="{ Authorization: `Bearer ${$store.state.user.token}` }"
+            v-bind:on-success="handleUplaodSuccess"
+          >
+          <!-- 上传成功，要预览的图片 -->
+            <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
+        </el-tab-pane>
+      </el-tabs>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
         <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
@@ -23,7 +41,8 @@ export default {
   name: 'UploadImage',
   data() {
     return {
-      dialogVisible: false
+      dialogVisible: false,
+      imageUrl: null
     }
   },
   methods: {
@@ -33,6 +52,9 @@ export default {
       // 2.在弹框中展示图片素材
       // 3.用户可以点击切换图片的选中状态
       // 4.用户点击确定，我们拿到所选的图片凝结，将数据给谁？
+    },
+    handleUplaodSuccess() {
+      console.log('上传成功')
     }
   }
 }
@@ -48,4 +70,27 @@ export default {
   align-items: center;
   flex-direction: column;
 }
+ .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
 </style>
