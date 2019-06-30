31cecba967f9e71d8e3dc20a8cf4687707275781
AngeviLee <568178364@qq.com>
2019-06-30 23:12:44 +0800

    封面处理-显示对话框

--- src/views/publish/components/upload-image.vue
+++ src/views/publish/components/upload-image.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
-    <div class="box-wrap">
+    <div class="box-wrap" @click="handleShowMediaBox">
       <p>点击上传图片</p>
       <i class="el-icon-upload" style="margin-top:20px
         font-size:90px;color:#eee">
         <img src="http://toutiao.meiduo.site/FvGUrO7Ru_T2ToQwn_OznfwKnjoL" width="120" />
       </i>
     </div>
+    <!-- 对话框 -->
+    <el-dialog title="素材" :visible.sync="dialogVisible" width="30%">
+      <span>这是一段信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -14,7 +22,18 @@
 export default {
   name: 'UploadImage',
   data() {
-    return {}
+    return {
+      dialogVisible: false
+    }
+  },
+  methods: {
+    handleShowMediaBox() {
+      // 1.弹出弹框
+      this.dialogVisible = true
+      // 2.在弹框中展示图片素材
+      // 3.用户可以点击切换图片的选中状态
+      // 4.用户点击确定，我们拿到所选的图片凝结，将数据给谁？
+    }
   }
 }
 </script>
